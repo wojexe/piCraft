6143edb2a6f22c7c67c23610068c4d483522a053
Sebastian Musial (Sebastianyyy) <64799476+Sebastianyyy@users.noreply.github.com>
2023-06-10 00:40:14 +0200

    basic configuration to handle files

--- backend/PicCraft/PicCraft/settings.py
+++ backend/PicCraft/PicCraft/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,12 +122,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 #A list of origins that are authorized to make cross-site HTTP requests
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
