e002ae37653d4e76ad4f5fe75369dd8dfbdb3074
adriancielniak <adriancielniak@gmail.com>
2023-06-15 00:55:26 +0200

    test_views edit

--- backend/PicCraft/tests/test_views.py
+++ backend/PicCraft/tests/test_views.py
@@ -8,7 +8,6 @@ from django.test import TestCase, Client
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 
-
 def temporary_image(format_img="png"):
     """
     Returns a new temporary image file
@@ -87,25 +86,25 @@ class MyTestCase(APITestCase):
         response = self.client.post(url, data=form_data, format="multipart")
         self.assertEqual(response.status_code, 200)
 
-    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
-    def test_bad_file_resize(self, format):
-        uploaded_file = temporary_image(format)
-        form_data = {
-            "params": """{"name": "resize", "width": "330", "height": "32"}""",
-            "file": uploaded_file,
-        }
-        url = "http://127.0.0.1:8000/resize/"
+    # @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    # def test_bad_file_resize(self, format):
+    #     uploaded_file = temporary_image(format)
+    #     form_data = {
+    #         "params": """{"name": "resize", "width": "330", "height": "32"}""",
+    #         "file": uploaded_file,
+    #     }
+    #     url = "http://127.0.0.1:8000/resize/"
+    #
+    #     response = self.client.post(url, data=form_data, format="multipart")
+    #     self.assertEqual(response.status_code, 200)
 
-        response = self.client.post(url, data=form_data, format="multipart")
-        self.assertEqual(response.status_code, 200)
-    
     # @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
     # def test_bad_format_resize(self, format):
     #     uploaded_file = temporary_image(format)
     #     form_data = {
-        # "params": """{"name": "resize", "width": "330", "height": "32"}""",
-        # "file": uploaded_file,
-        # }
+    # "params": """{"name": "resize", "width": "330", "height": "32"}""",
+    # "file": uploaded_file,
+    # }
     #     url = "http://127.0.0.1:8000/resize/"
 
     #     response = self.client.post(url, data=form_data, format="multipart")
@@ -115,10 +114,134 @@ class MyTestCase(APITestCase):
     # def test_bad_form_data(self, format):
     #     uploaded_file = temporary_image(format)
     #     form_data = {
-        # "params": """{"name": "resize", "width": "330", "height": "32"}""",
-        # "file": uploaded_file,
-        # }
+    # "params": """{"name": "resize", "width": "330", "height": "32"}""",
+    # "file": uploaded_file,
+    # }
     #     url = "http://127.0.0.1:8000/resize/"
 
     #     response = self.client.post(url, data=form_data, format="multipart")
     #     self.assertEqual(response.status_code, 200)
+
+    # compress
+    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    def test_bad_name_compress(self, format):
+        uploaded_file = temporary_image(format)
+        form_data = {
+            "params": '{"name": "compres", "rate": "50"}',
+            "file": uploaded_file,
+        }
+        url = "http://127.0.0.1:8000/resize/"
+
+        response = self.client.post(url, data=form_data, format="multipart")
+        self.assertEqual(response.status_code, 400)
+
+    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    def test_bad_small_rate_compress(self, format):
+        uploaded_file = temporary_image(format)
+        form_data = {
+            "params": '{"name": "compress", "rate": "3"}',
+            "file": uploaded_file,
+        }
+        url = "http://127.0.0.1:8000/resize/"
+
+        response = self.client.post(url, data=form_data, format="multipart")
+        self.assertEqual(response.status_code, 400)
+
+    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    def test_bad_small_rate_compress(self, format):
+        uploaded_file = temporary_image(format)
+        form_data = {
+            "params": '{"name": "compress", "rate": "3"}',
+            "file": uploaded_file,
+        }
+        url = "http://127.0.0.1:8000/resize/"
+
+        response = self.client.post(url, data=form_data, format="multipart")
+        self.assertEqual(response.status_code, 400)
+
+    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    def test_bad_big_rate_compress(self, format):
+        uploaded_file = temporary_image(format)
+        form_data = {
+            "params": '{"name": "compress", "rate": "169"}',
+            "file": uploaded_file,
+        }
+        url = "http://127.0.0.1:8000/resize/"
+
+        response = self.client.post(url, data=form_data, format="multipart")
+        self.assertEqual(response.status_code, 400)
+
+    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    def test_good_compress(self, format):
+        uploaded_file = temporary_image(format)
+        form_data = {
+            "params": '{"name": "compress", "rate": "69"}',
+            "file": uploaded_file,
+        }
+        url = "http://127.0.0.1:8000/resize/"
+
+        response = self.client.post(url, data=form_data, format="multipart")
+        self.assertEqual(response.status_code, 200)
+
+    # enhance
+    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    def test_bad_name_enhance(self, format):
+        uploaded_file = temporary_image(format)
+        form_data = {
+            "params": '{"name": "enhence"}',
+            "file": uploaded_file,
+        }
+        url = "http://127.0.0.1:8000/resize/"
+
+        response = self.client.post(url, data=form_data, format="multipart")
+        self.assertEqual(response.status_code, 400)
+
+    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    def test_good_enhance(self, format):
+        uploaded_file = temporary_image(format)
+        form_data = {
+            "params": '{"name": "enhance"}',
+            "file": uploaded_file,
+        }
+        url = "http://127.0.0.1:8000/resize/"
+
+        response = self.client.post(url, data=form_data, format="multipart")
+        self.assertEqual(response.status_code, 200)
+
+    # change_format
+
+    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    def test_change_format_bad_name(self, format):
+        uploaded_file = temporary_image(format)
+        form_data = {
+            "params": '{"name": "changeformat", "format": "png"}',
+            "file": uploaded_file,
+        }
+        url = "http://127.0.0.1:8000/resize/"
+
+        response = self.client.post(url, data=form_data, format="multipart")
+        self.assertEqual(response.status_code, 400)
+
+    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    def test_change_format_bad_format(self, format):
+        uploaded_file = temporary_image(format)
+        form_data = {
+            "params": '{"name": "change_format", "format": "pns"}',
+            "file": uploaded_file,
+        }
+        url = "http://127.0.0.1:8000/resize/"
+
+        response = self.client.post(url, data=form_data, format="multipart")
+        self.assertEqual(response.status_code, 400)
+
+    @parameterized.expand([('jpg'), ('jpeg'), ('png'), ('heic'), ('gif'), ('tiff'), ('bmp'), ('webp')])
+    def test_change_format_good(self, format):
+        uploaded_file = temporary_image(format)
+        form_data = {
+            "params": '{"name": "change_format", "format": "png"}',
+            "file": uploaded_file,
+        }
+        url = "http://127.0.0.1:8000/resize/"
+
+        response = self.client.post(url, data=form_data, format="multipart")
+        self.assertEqual(response.status_code, 200)
